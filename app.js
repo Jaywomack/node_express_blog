28b1629aae3a97bcc1ea59e082a7024e41e9a238
jaywomack <womackjw@hotmail.com>
2021-05-27 12:47:32 -0700

    Finished

--- app.js
+++ app.js
@@ -1,47 +1,46 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const blogRoutes = require('./routes/BlogRoutes');
 
-//express app
+const dotenv = require('dotenv');
+dotenv.config();
+// express app
 const app = express();
+
+// connect to mongodb & listen for requests
+const dbURI = process.env.HOST;
+
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
 
 // register view engine
 app.set('view engine', 'ejs');
 
-// listen for requests
-app.listen(3000);
-
-// middleware && static files
+// middleware & static files
 app.use(express.static('public'));
-
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
+});
 
+// routes
 app.get('/', (req, res) => {
-  const blogs = [
-    {
-      title: 'Yoshi finds eggs',
-      snippet: 'These are the words in the snippet'
-    },
-    {
-      title: 'Mario finds stars',
-      snippet: 'These are the words in the snippet'
-    },
-    {
-      title: 'How to Defeat Bowser',
-      snippet: 'These are the words in the snippet'
-    }
-  ];
-  res.render('index', { title: 'Home', blogs: blogs });
+  res.redirect('/blogs');
 });
 
 app.get('/about', (req, res) => {
   res.render('about', { title: 'About' });
 });
 
-app.get('/blogs/create', (req, res) => {
-  res.render('create', { title: 'Create A Blog' });
-});
+// blog routes
+app.use(blogRoutes);
 
 // 404 page
 app.use((req, res) => {
-  res.status(404).render('404', { title: '404 | Page Not Found' });
+  res.status(404).render('404', { title: '404' });
 });
